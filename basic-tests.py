cc89addeb0fd1e710d102008c7907872f69941b0
John Clements <clements@racket-lang.org>
2024-02-05 16:32:23 -0800

    refine basic_tests

--- basic-tests.py
+++ basic-tests.py
@@ -1,6 +1,9 @@
 import unittest
 import math
-from main import HLeaf, tree_lt, base_tree_list, tree_list_insert, initial_tree_sort, coalesce_once, coalesce_all
+import numpy as np
+from main import HLeaf, HNode, HTNode, tree_lt, base_tree_list, tree_list_insert, \
+    initial_tree_sort, coalesce_once, coalesce_all, cnt_freq, build_encoder_array, \
+    encode_string_one, bits_to_chars
 
 # please note: these are not real tests! They don't include
 # expected results, and they don't actually test that the
@@ -18,14 +21,47 @@ from main import HLeaf, tree_lt, base_tree_list, tree_list_insert, initial_tree_
 
 class MyTests(unittest.TestCase):
 
-    def test_tree_lt_existence(self):
-        tree_lt(HLeaf('a',0),HLeaf('a',0))
-        base_tree_list([0 for i in range(256)])
-        tree_list_insert([])
-        initial_tree_sort([])
-        coalesce_once([HLeaf('a',0),HLeaf('a',0)])
-        coalesce_all([HLeaf('a',0)])
+    def test_cnt_freq(self):
+        table = cnt_freq("abcd")
+        table[3]
+        table[79]
 
+    def test_tree_construction(self):
+        HNode(3,'a',HLeaf(1,'a'),HLeaf(2,'b'))
+
+    def test_tree_lt(self):
+        self.assertEqual(type(tree_lt(HLeaf(0,'a'),HLeaf(0,'a'))), bool)
+
+    def test_tree_lists(self):
+        HTNode(HLeaf(0,'a'),HTNode(HLeaf(0,'a'),None))
+
+    def test_base_tree_list(self):
+        self.assertEqual(type(base_tree_list(np.array([0 for i in range(256)],dtype=np.int32))),HTNode)
+
+    def test_tree_lt_insert(self):
+        self.assertEqual(type(tree_list_insert(None,HLeaf(0,'a'))),HTNode)
+
+    def test_initial_tree_sort(self):
+        self.assertEqual(type(initial_tree_sort(HTNode(HLeaf(0,'a'),None))),HTNode)
+
+    def test_coalesce_once(self):
+        self.assertEqual(type(coalesce_once(HTNode(HLeaf(0,'a'),HTNode(HLeaf(0,'a'),None)))),HTNode)
+
+    def test_coalesce_all(self):
+        self.assertEqual(type(coalesce_all(HTNode(HLeaf(0,'a'),HTNode(HLeaf(0,'a'),None)))),HNode)
+
+    def test_build_encoder_array(self):
+        build_encoder_array(HLeaf(0,'a'),np.array(["" for i in range(256)],dtype='object'),"")
+
+    def test_encode_string_one(self):
+        self.assertEqual(type(encode_string_one("abcd",
+                                           np.array(["" for i in range(256)],dtype='object'))),
+                         str)
+
+    def test_bits_to_chars(self):
+        self.assertEqual(type(bits_to_chars("abcd")),str)
 
 if (__name__ == '__main__'):
     unittest.main()
+    MyTests().test_tree_lists()
+    #HTNode(HLeaf(0, 'a'), HTNode)
